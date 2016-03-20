6e80119e9c8f7e23f32b657b67dcd5cf1066d090
Matthew <matthew.t.kides@cox.net>
2016-03-19 18:49:57 -0700

    Added ui bar and text scripts.

---
+++ Assets/Resources/Scripts/UI/valueBar.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class valueBar : MonoBehaviour
+{
+    [SerializeField]
+    Image uiImage; //reference to the Image for the UI Bar
+
+    public float minValue; //the minimum value for the bar
+    public float currentValue; //the current value of the bar
+    public float maxValue; //the maximum value for the bar
+
+    
+    public bool colorFade; //whether or not to fade the colors for the bar.
+    public Color normalColor; //the color the bar should use if it is not fading.
+    public Color fullColor; //the color to use when the bar is full.
+    public Color emptyColor; //the color to use when the bar is empty.
+
+    //the last value of the bar. Used to check if the value of the bar changes before updating it.
+    float lastValue; 
+
+    /// <summary>
+    /// Unity's Start Function
+    /// </summary>
+    void Start()
+    {
+        //set the last value to not equal current value to start.
+        lastValue = currentValue - 1;
+
+        //if the image isin't set already try and get it from the object this is attached to.
+        if (!uiImage)
+        {
+            uiImage = GetComponent<Image>();
+        }
+    }
+
+    /// <summary>
+    /// Unity's Update Function
+    /// </summary>
+    void Update()
+    {
+        //check if the value of the bar has changed before updating.
+        if (currentValue != lastValue && uiImage)
+        {
+            //change the scale to match the ratio of the current and the max adjusted based on the min.
+            transform.localScale = new Vector3((currentValue - minValue) / (maxValue - minValue), 1.0f, 1.0f);
+
+            //fade the color based on the given full and empty colors.
+            if (colorFade)
+                uiImage.color = Color.Lerp(emptyColor, fullColor, transform.localScale.x);
+            else
+                uiImage.color = normalColor;
+        }
+
+        lastValue = currentValue;
+    }
+}
