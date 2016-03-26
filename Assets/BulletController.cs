bd1509a6fccdaf043a8c9b70f65145b0e4490253
adam444888 <zippydave@comcast.net>
2016-03-26 10:21:21 -0400

    Made a bullet object, and allowed the player to shoot with the left click.

---
+++ Assets/BulletController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour 
+{
+    [SerializeField]
+    private int speed = 5;
+
+    [SerializeField]
+    private float bulletTime = 3;
+
+    private float bulletTimer = 0;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position += transform.up * speed * Time.deltaTime;
+        if (bulletTimer >= bulletTime)
+        {
+            Destroy(this.gameObject);
+        }
+        bulletTimer += Time.deltaTime;
+	}
+}
--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -5,21 +5,28 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5;
+    [SerializeField]
+    private int shotTimer = 50;
+    [SerializeField]
+    private GameObject bullet;
 
     private Vector2 position;
     private float rotation;
     private Vector3 mousePosition;
+    public float timer;
 
 	void Start () 
     {
         position.x = transform.position.x;
         position.y = transform.position.y;
+        timer = 0;
 	}
 
 	void Update () 
     {
         Move();
         FaceMouse();
+        Shoot();
 	}
 
     void Move() //Allows the player to move using wasd, speed stat effects the speed.
@@ -51,5 +58,18 @@ public class PlayerController : MonoBehaviour
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); //Converts the mouses screen position to a position within the world space
         transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePosition - transform.position); //Assumes sprite is facing up can be changed.
+    }
+
+    void Shoot()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (timer >= shotTimer)
+            {
+                Instantiate(bullet, transform.position, transform.rotation);
+                timer = 0;
+            }
+        }
+        timer += 1 + Time.deltaTime;
     }
 }
