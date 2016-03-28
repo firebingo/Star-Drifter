15c64ca66236f0d7866e1f80e7da19c520755aca
Allenjonesing <Allenjonesing@gmail.com>
2016-03-27 23:49:06 -0500

    Added some basic functions for the Ai like taking stimuli, weighing aggression, and gauging target distance.

--- Assets/Resources/Scripts/AI/Occupant.cs
+++ Assets/Resources/Scripts/AI/Occupant.cs
@@ -3,10 +3,12 @@
 // Date: 3-26-16
 // Version: 1.0
 // Description: This code runs the Occupant AI FSM
+// Allen:3/27:  Added aggression and stimuli functions. removed bullet object.
 //==========================================================
 
 using UnityEngine;
 using System.Collections;
+using System; //for math
 
 public class Occupant : MonoBehaviour
 {
@@ -14,51 +16,66 @@ public class Occupant : MonoBehaviour
     private float speed = 5;
     [SerializeField]
     private int shotTimer = 50;
-    [SerializeField]
-    private GameObject bullet;
+ 
 
     //Variables
     private Vector2 position;
+    private GameObject target;  //Current target ('GameObject' should be 'object'?)
 
-    private float rotation;
     public float fireTimer;
 
     private int health;
     private int weaponHeld;
+    private int aggression;
 
     private float velocity;
-
+    private float rotation;
+    private float targDist;     //Distance from target
 
     //enum for FSM
-    public enum AIFSM  {Wander, Attack, Wait}
+    public enum AIFSM  {Wander, Attack, Wait, Avoid}
     public AIFSM AIState = AIFSM.Wait;
+    private object distance;
 
+    /// <summary>
+    /// Gather info such as player distance, nearest obstacle, and nerest target
+    /// </summary>
+    void TakeStim()
+    {
+        //Get target distance
+        TargDist(target);
 
+        //DO stuff...
+    }
 
     /// <summary>
     /// AI bot is awake
     /// </summary>
-    void Awake() { }
+    void Awake() { /*Do stuff?*/ }
     
     /// <summary>
     ///  Use this for initialization
     /// </summary>
-    void Init ()
+    void init ()
     {
         health = 100;
         fireTimer = 0;
         weaponHeld = 0;
+        aggression = 0;
 
         position.x = transform.position.x;
         position.y = transform.position.y;
 
         velocity = 0;
+        rotation = 0;
             
     }
 
+    //void 
+
     /// <summary>
     /// Update is called once per frame
-    /// </summary>
+    /// </summary>  
     void Update ()
     {
         
@@ -70,14 +87,26 @@ public class Occupant : MonoBehaviour
                 break;
             case AIFSM.Wait:
                 break;
+            case AIFSM.Avoid:
+                break;
 
         }//end switch
 
 	}//end update
     
-    //State Machine Functions
+    /// <summary>
+    /// Distance function to target
+    /// </summary>
+    void TargDist(GameObject target)
+    {
+        //Needs work
+        //distance = Math.Sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+    }
+
+    //State Machine Functions (per update)
     private void Wander() { }//Patrol
     private void Attack() { }//Attack
     private void Wait() { }//Flee
+    private void Avoid() { }//Avoid nearby obstacle
 
 }
