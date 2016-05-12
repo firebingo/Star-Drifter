0ef65e79e64c9db136475a2482c49b23301cc010
adam444888 <zippydave@comcast.net>
2016-05-12 11:52:02 -0400

    Created the start to the building script. Currently uses 6-8 to instantiate tiles. To Do: Have the tiles fall into a grid to make it easy to place them together, changing tile placing to be based upon an interface when its created, adding a resource cost to placing tiles.

---
+++ Assets/Resources/Scripts/Building.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour 
+{
+    GameObject tile;
+
+
+    Vector2 coords;
+
+	// Update is called once per frame
+	void Update() 
+    {
+	    //if (Input.GetKeyDown(KeyCode.Alpha6))
+        if (Input.anyKeyDown)
+        {
+            switch (Input.inputString)
+            {
+                case "6":
+                    tile = Resources.Load("Prefabs/Tile_0") as GameObject;
+                    coords = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    Instantiate(tile, coords, tile.transform.rotation);
+                    break;
+                case "7":
+                    tile = Resources.Load("Prefabs/Tile_1") as GameObject;
+                    coords = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    Instantiate(tile, coords, tile.transform.rotation);
+                    break;
+                case "8":
+                    tile = Resources.Load("Prefabs/Tile_2") as GameObject;
+                    coords = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    Instantiate(tile, coords, tile.transform.rotation);
+                    break;
+            }
+        }
+	}
+}
