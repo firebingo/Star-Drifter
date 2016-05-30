467c3eb15868379c94d6f0fa1aa2c76a974bc5fc
adam444888 <zippydave@comcast.net>
2016-05-29 22:23:52 -0400

    Improved the build system by making so tiles can't be placed on each other. (this feature broke earlier when making the tile follow the mouse till clicking).

--- Assets/Resources/Scripts/Building.cs
+++ Assets/Resources/Scripts/Building.cs
@@ -30,40 +30,50 @@ public class Building : MonoBehaviour
             switch (Input.inputString)
             {
                 case "6":
-                    if (!Physics2D.Raycast(new Vector2(Mathf.Round(coords.x), Mathf.Round(coords.y - 1)), Vector2.up, 1.5f, LayerMask.GetMask("Tile")))//Checking if object is being spawned on another tile (based on layer).
-                    {
+                         if (building == true)
+                            Destroy(ghost.gameObject);
                         tile = Resources.Load("Prefabs/Tile_0") as GameObject;
                         ghost = (GameObject)Instantiate(tile, new Vector3(Mathf.Round(coords.x), Mathf.Round(coords.y), 0), tile.transform.rotation);
                         ghost.GetComponent<SpriteRenderer>().color = Color.Lerp(ghost.GetComponent<SpriteRenderer>().color, new Color(ghost.GetComponent<SpriteRenderer>().color.r, ghost.GetComponent<SpriteRenderer>().color.b, ghost.GetComponent<SpriteRenderer>().color.g, 0.5f), 1);
+                        ghost.gameObject.layer = 13; //Sets the ghost tile to no collisions
                         building = true;
-                    }
-                    break;
+                        break;
                 case "7":
-                    if (!Physics2D.Raycast(new Vector2(Mathf.Round(coords.x), Mathf.Round(coords.y - 1)), Vector2.up, 1.5f, LayerMask.GetMask("Tile")))//Checking if object is being spawned on another tile (based on layer).
-                    {
+                        if (building == true)
+                            Destroy(ghost.gameObject);
                         tile = Resources.Load("Prefabs/Tile_1") as GameObject;
                         ghost = (GameObject)Instantiate(tile, new Vector3(Mathf.Round(coords.x), Mathf.Round(coords.y), 0), tile.transform.rotation);
                         ghost.GetComponent<SpriteRenderer>().color = Color.Lerp(ghost.GetComponent<SpriteRenderer>().color, new Color(ghost.GetComponent<SpriteRenderer>().color.r, ghost.GetComponent<SpriteRenderer>().color.b, ghost.GetComponent<SpriteRenderer>().color.g, 0.5f), 1);
+                        ghost.gameObject.layer = 13; //Sets the ghost tile to no collisions
                         building = true;
-                    }
-                    break;
+                        break;
                 case "8":
-                    if (!Physics2D.Raycast(new Vector2(Mathf.Round(coords.x), Mathf.Round(coords.y - 1)), Vector2.up, 1.5f, LayerMask.GetMask("Tile")))//Checking if object is being spawned on another tile (based on layer).
-                    {
+                        if (building == true)
+                            Destroy(ghost.gameObject);
                         tile = Resources.Load("Prefabs/Tile_2") as GameObject;
                         ghost = (GameObject)Instantiate(tile, new Vector3(Mathf.Round(coords.x), Mathf.Round(coords.y), 0), tile.transform.rotation);
                         ghost.GetComponent<SpriteRenderer>().color = Color.Lerp(ghost.GetComponent<SpriteRenderer>().color, new Color(ghost.GetComponent<SpriteRenderer>().color.r, ghost.GetComponent<SpriteRenderer>().color.b, ghost.GetComponent<SpriteRenderer>().color.g, 0.5f), 1);
+                        ghost.gameObject.layer = 13; //Sets the ghost tile to no collisions
                         building = true;
-                    }
-                    break;
+                        break;
             }
 
             if (Input.GetButton("Fire"))
             {
-                if (ghost != null)
-                    ghost.GetComponent<SpriteRenderer>().color = Color.Lerp(ghost.GetComponent<SpriteRenderer>().color, new Color(ghost.GetComponent<SpriteRenderer>().color.r, ghost.GetComponent<SpriteRenderer>().color.b, ghost.GetComponent<SpriteRenderer>().color.g, 1), 1);
-                ghost = null;
-                startTimer = true;
+                if (!Physics2D.Raycast(new Vector2(Mathf.Round(coords.x), Mathf.Round(coords.y - 1)), Vector2.up, 1.5f, LayerMask.GetMask("Tile")))
+                {
+                    if (ghost != null)
+                    {
+                        ghost.GetComponent<SpriteRenderer>().color = Color.Lerp(ghost.GetComponent<SpriteRenderer>().color, new Color(ghost.GetComponent<SpriteRenderer>().color.r, ghost.GetComponent<SpriteRenderer>().color.b, ghost.GetComponent<SpriteRenderer>().color.g, 1), 1);
+                        ghost.gameObject.layer = 12; //Puts the tile back in the Tile layer when placed.
+                    }
+
+                    if (building == true && ghost != null)
+                    {
+                        startTimer = true;
+                    }
+                    ghost = null;
+                }
             }
         }
 
