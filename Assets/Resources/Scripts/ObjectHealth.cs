72bb5b162c49ef7968c212ac96c1b2381488d925
adam444888 <zippydave@comcast.net>
2016-06-20 22:23:04 -0400

    Added in a bunch of prefabs. MiscObjects refer to the things within stations, and the turrets are for the turrets. Added in an object health script as well so they are destroyable (basically just the enemy health script without the drops).

---
+++ Assets/Resources/Scripts/ObjectHealth.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectHealth : MonoBehaviour 
+{
+   public  float maxHealth = 500.0f;
+   public float currentHealth;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    currentHealth = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        if (currentHealth <= 0)
+        {
+            Destroy(this.gameObject);
+
+        } 
+
+
+	}
+
+    void ApplyDamage(float damage)
+    {
+        currentHealth -= damage;
+    }
+
+}
+
