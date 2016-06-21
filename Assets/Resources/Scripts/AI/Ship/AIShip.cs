cc2d4e3e43c6a36c42417c729105c0364bdd58f8
K45PIERCE <casper2t22@yahoo.com>
2016-06-20 18:28:08 -0700

    Ships moves to point

--- Assets/Resources/Scripts/AI/Ship/AIShip.cs
+++ Assets/Resources/Scripts/AI/Ship/AIShip.cs
@@ -3,17 +3,27 @@ using System.Collections;
 
 public class AIShip : MonoBehaviour {
 
-    public bool boreded = true;
+    public bool boreded = false;
     public int maxSpace = 5;
     int numSpawn = 0;
     public int passengers = 0;
     private GameObject Assassin, Warrior, temp;
-    float delay = 5.0f;
+    float delay = 4.0f;
     int id = 0;
     float timePassed = 0.0f;
+    Vector3 landingPoint = new Vector3(0.5f,-7f,1f);
+    float speed = 1.0f;
+   public float distance;
 
-	// Use this for initialization
-	void Start () {
+
+
+    //temporary Vectors
+    Vector3 targetV, enemyV, NLinear;
+
+
+
+    // Use this for initialization
+    void Start () {
         this.Assassin = Resources.Load("Prefabs/Enemy_Assassin") as GameObject;
         this.Warrior = Resources.Load("Prefabs/Enemy_Warrior") as GameObject;
 
@@ -24,12 +34,16 @@ public class AIShip : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+       
+        if (!boreded) {
+            MoveToDeboard();
 
-
-        if (boreded)
+            distance = Mathf.Abs(Vector3.Distance(transform.position, landingPoint));
+            if (distance <= 1.5f) { boreded = true; }
+        }
+        else if (boreded)
         { SpawnEnemies(); }
-        else
-        { }
+       
 
 
         
@@ -66,8 +80,30 @@ public class AIShip : MonoBehaviour {
     }
 
     void MoveToDeboard() {
-
+        rotateForward(landingPoint);
+        targetV = new Vector3(landingPoint.x, landingPoint.y, 0);
+        enemyV = new Vector3(transform.position.x, transform.position.y, 0);
+        //NLinear = Norm(enemyV, targetV);
+        NLinear = Norm(targetV, enemyV);
+        transform.position += (NLinear * speed * Time.deltaTime);
 
     }
+    //normalize vectors
+    private Vector3 Norm(Vector3 target, Vector3 position)
+    {
+        Vector3 linear = target - position;
+        linear = linear.normalized;
+        return linear;
+    }//Norm()
 
+    //Ensure the enemy is facing direction it is moving in
+    private void rotateForward(Vector3 target)
+    {
+        Vector3 dir = target - transform.position;
+        if (dir != Vector3.zero)
+        {
+            float angle = Mathf.Atan2(-dir.x, dir.y) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }//rotateforward
 }
